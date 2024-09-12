db1a429015b7ca022a3dfc8f262ad963cf953369
DanielBata25 <dfbh2006@gmail.com>
2024-09-12 03:39:16 -0500

    RETOQUES_NIVELES

--- Video-juego/js/tiempoEspera.js
+++ Video-juego/js/tiempoEspera.js
@@ -1,6 +1,13 @@
 function timeEspera(){
     setTimeout(function(){
-        window.location.href='numeros.html';
-    },10000);
+        window.location.href='Result_Racional.html';
+    },5000);
 }
 timeEspera();
+
+function timeEspera2(){
+    setTimeout(function(){
+        window.location.href='Result_Suma.html';
+    },5000);
+}
+timeEspera2();
