01d2b949bef2e726154946d085462d0d5c774bb0
JuanDavid303 <juadtovar1@soy.sena.edu.co>
2024-10-10 21:34:57 -0500

    Responsive en las carpetas

--- Video-juego/js/tiempo.js/imagenes/estrella.js
+++ Video-juego/js/tiempo.js/imagenes/estrella.js
@@ -1,6 +1,6 @@
-function timeEspera(){
+  function timeEspera(){
     setTimeout(function(){
        window.location.href='../../Resultados/imagenes/estrella.html'
     },7000);
 }
-timeEspera();
+timeEspera(); 
--- Video-juego/js/tiempo.js/imagenes/profesional.js
+++ Video-juego/js/tiempo.js/imagenes/profesional.js
@@ -1,6 +1,6 @@
 function timeEspera(){
-    setTimeout(function(){
-       window.location.href='../../Resultados/imagenes/profesional.html'
+  setTimeout(function(){
+      window.location.href='../../Resultados/imagenes/profesional.html'
     },7000);
 }
-timeEspera();
+timeEspera(); 
